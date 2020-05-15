439c25646cdca0f1607be01e6fbbc1f19b8f8513
supriyo-ux <roybanerjee.supriyo@gmail.com>
2020-05-15 16:01:14 +0530

    config-repo

---
+++ Spring-Cloud-Config/spring-config-client/spring-config-client/src/test/java/com/spring/supriyo/springconfigclient/SpringConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.supriyo.springconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
